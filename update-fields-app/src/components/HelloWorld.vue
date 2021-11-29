2240d3dfd50b8a2bd86d84c13cae1c4015b073ec
Eli <eli.h.schei@outlook.com>
2021-11-29 08:35:09 +0100

    Added auth and upload logic

--- update-fields-app/src/components/HelloWorld.vue
+++ update-fields-app/src/components/HelloWorld.vue
@@ -1,40 +1,110 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="container demo-wrapper">
+    <h1>{{ msg }}</h1>
+    <form id="uploadDocForm" class="container">
+      <div class="row">
+        <label class="col-3 col-md-3 col-sm-12">Date </label>
+        <div class="col-3 col-md-3 col-sm-12">
+          <input required id="customDate" type="date" />
+        </div>
+      </div>
+      <div class="row">
+        <label class="col-3 col-md-3 col-sm-12">Type:</label>
+        <div class="col-3 col-md-3 col-sm-12">
+          <select required id="customType" name="customType">
+            <option value="" selected disabled hidden>Select a type</option>
+            <option
+              v-for="(customType, i) in typeValues"
+              v-bind:key="'thistype' + i"
+              v-bind:value="customType"
+            >
+              {{ customType }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <label class="col-3 col-md-3 col-sm-12">
+            Another value:
+        </label>
+        <div class="col-3 col-md-3 col-sm-12">
+          <input id="anotherValue" name="anotherValue">
+        </div>       
+      </div>
+      <div class="row selectFileArea">
+        <div class="col-3 col-md-3 col-sm-12">
+          <input id="selectedFile" type="file" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <button type="submit" form="uploadDocForm" v-on:click="onSubmit">
+            Upload file to SharePoint
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
+<script>
+import { signInWithMsal } from "../authenticatingWithMsal";
+import { uploadDocument } from "../uploadingDocWithGraph";
+import { typeValues } from "../fieldSelectValues";
+export default {
+  name: "UploadIncidentForm",
+  props: ["msg"],
+  setup() {
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      await signInWithMsal();
+      const filename = document.getElementById("selectedFile").files[0].name;
+      const fileToUplode = document.getElementById("selectedFile").files[0];
+      const filereader = new FileReader();
+      filereader.onload = async (event) => {
+        await uploadDocument(filename, event.target.result);
+      };
+      filereader.readAsArrayBuffer(fileToUplode);
+    };
+    return {
+      onSubmit,
+      typeValues,
+    };
+  },
+};
+</script>
+
+<style>
+.demo-wrapper {
+  text-align: left;
+  border: 1px solid grey;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+.demo-wrapper h1 {
+  margin-bottom: 30px;
+}
+
+.demo-wrapper .row {
+  margin-bottom: 15px;
+  text-align: left;
+}
+
+.demo-wrapper button{
+  padding: 10px;
+  border-radius: 10px;
+  background: #aed1ca;
+  color: black;
+  border: none;
+}
+.selectFileArea{
+  margin-top: 20px;
+  padding-top: 20px;
+  border-top: 1px solid #aed1ca;
+}
+.demo-wrapper button:hover{
+  background: #333333;
+  color: white;
 }
 </style>
